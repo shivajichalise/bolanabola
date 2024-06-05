16b910a28bf0673904da06941164e6f390b0cdb7
Shivaji Chalise <respond2shivaji@gmail.com>
2024-06-05 09:17:35 +0545

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -100,7 +100,7 @@ function App() {
                                             )
                                         }
                                     >
-                                        <div className="avatar online mr-3">
+                                        <div className="avatar mr-3">
                                             <div className="w-10 rounded-full">
                                                 <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                                             </div>
